f8cc22bbcdeeae9f3548ad72de1a27b87e53404e
Your Name <you@example.com>
2021-11-28 15:39:17 +0900

    added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # post_listという名前のビューをルートURLに割り当てる
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
